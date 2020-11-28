1ae621e29d26e9f897e9b75ccc0719bdf33294d8
araawi7 <62933812+araawi7@users.noreply.github.com>
2020-11-28 16:20:20 +0300

    Update route_name.dart

--- lib/routes/route_name.dart
+++ lib/routes/route_name.dart
@@ -6,7 +6,6 @@ const String forgetPageRoute='Forget Page';
 const String mainPageRoute= 'Main Page';
 const String foaterPageRoute= 'Foater Page';
 const String settingPageRoute= 'Setting Page';
-const String accountPageRoute= 'Account Page';
 const String contactPageRoute= 'Contact Page';
 const String aboutPageRoute= 'About Page';
 const String privacyPageRoute= 'Privacy Page';
