75f868f205e1b87724935970c9c269dfadd94ab6
araawi7 <62933812+araawi7@users.noreply.github.com>
2020-11-28 16:23:48 +0300

    Update login_page.dart

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,7 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fwatery/routes/route_name.dart';
-
 import 'main_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,14 +11,18 @@ class LoginPage extends StatefulWidget {
 
 class _LoginViewState extends State<LoginPage> {
   FirebaseAuth _auth = FirebaseAuth.instance;
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String _error; //added by mona
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-
+    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text(
           "Login",
@@ -28,116 +32,129 @@ class _LoginViewState extends State<LoginPage> {
         backgroundColor: Colors.orangeAccent,
       ),
       body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: PageView(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 16, right: 16),
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    TextFormField(
-                      validator: (value) {
-                        if (value.isEmpty) return 'Please enter valid Email';
-                        return null;
-                      },
-                      controller: _emailController,
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                      cursorColor: Colors.grey,
-                      decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.grey,
-                          ),
-                        ),
-                        hintText: "something@example.com",
-                        labelText: "Email",
-                        labelStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    TextFormField(
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Please enter Password';
-                        }
-                        return null;
-                      },
-                      obscureText: true,
-                      controller: _passwordController,
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                      cursorColor: Colors.grey,
-                      decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.grey,
-                          ),
-                        ),
-                        hintText: "password",
-                        labelText: "Password",
-                        labelStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        FlatButton(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          onPressed: () {
-                            Navigator.pushNamed(context, forgetPageRoute);
-                          },
-                          child: Text("Forget Password",
-                              style: TextStyle(
-                                  fontSize: 13, color: Colors.orangeAccent)),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
                     Container(
-                      width: 250,
-                      height: 40,
-                      margin: EdgeInsets.symmetric(horizontal: 80),
-                      child: OutlineButton(
-                        padding: EdgeInsets.symmetric(horizontal: 40),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Text("Log in",
-                            style: TextStyle(
-                                fontSize: 16,
-                                letterSpacing: 2.2,
-                                color: Colors.orangeAccent)),
-                        onPressed: () async {if (
-                        _formKey.currentState.validate()) {
-                          _signInWithEmailAndPassword();
-                        }},
+                      padding: EdgeInsets.only(left: 16, right: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          showAlert(),
+                          SizedBox(height: _height * 0.025),
+                          TextFormField(
+                            controller: _emailController,
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return 'email can not be empty!';
+                              }
+                              if (!value.contains("@")) {
+                                return 'Please enter valid Email like:something@example.com';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              hintText: "something@example.com",
+                              labelText: "Email",
+                              labelStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          TextFormField(
+                            controller: _passwordController,
+                            validator: (String value) {
+                              if (value.isEmpty) {
+                                return 'password can not be empty!';
+                              }
+                              if (value.length < 8) {
+                                return 'password length must be 8 or more digit';
+                              }
+                              return null;
+                            },
+                            obscureText: true,
+                            cursorColor: Colors.black,
+                            decoration: InputDecoration(
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              hintText: "password",
+                              labelText: "Password",
+                              labelStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              FlatButton(
+                                padding: EdgeInsets.symmetric(horizontal: 40),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                onPressed: () {
+                                  Navigator.pushNamed(context, forgetPageRoute);
+                                },
+                                child: Text("Forget Password",
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.orangeAccent)),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: 250,
+                            height: 40,
+                            margin: EdgeInsets.symmetric(horizontal: 80),
+                            child: OutlineButton(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text("Log in",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      letterSpacing: 2.2,
+                                      color: Colors.orangeAccent)),
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  _signInWithEmailAndPassword();
+                                }
+                                //Scaffold.of(context).showSnackBar(SnackBar(content: Text('the email or password is not correct')));
+                              },
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );
@@ -148,6 +165,44 @@ class _LoginViewState extends State<LoginPage> {
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
+  }
+
+  Widget showAlert() {
+    if (_error != null) {
+      return Container(
+        color: Colors.grey,
+        width: double.infinity,
+        padding: EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.error_outline),
+            ),
+            Expanded(
+              child: AutoSizeText(
+                _error,
+                maxLines: 3,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    _error = null;
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      );
+    }
+    return SizedBox(
+      height: 0,
+    );
   }
 
   void _signInWithEmailAndPassword() async {
@@ -164,11 +219,10 @@ class _LoginViewState extends State<LoginPage> {
         return MainPage();
       }));
     } catch (e) {
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text("Failed to sign in with Email & Password"),
-      ));
+      print(e);
+      setState(() {
+        _error = e.message;
+      });
     }
   }
-
-
 }
