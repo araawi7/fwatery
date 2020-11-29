9f9e2c03055678a8a42109ce9ab6b893c8a09689
araawi7 <62933812+araawi7@users.noreply.github.com>
2020-11-29 11:30:30 +0300

    Update fwatery_page.dart

--- lib/Pages/fwatery_page.dart
+++ lib/Pages/fwatery_page.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
+
 class FoaterPage extends StatefulWidget {
   final String url;
 
@@ -8,6 +8,7 @@ class FoaterPage extends StatefulWidget {
   @override
   _FoaterPageState createState() => _FoaterPageState(url);
 }
+
 class _FoaterPageState extends State<FoaterPage> {
   final String url;
 
@@ -15,29 +16,13 @@ class _FoaterPageState extends State<FoaterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar:AppBar(
-            title: Text('Fwatery'),
-            backgroundColor: Colors.orangeAccent
-        ),
-        body:StreamBuilder(
-          stream: FirebaseFirestore.instance.collection("Pdf").snapshots(),
-          builder: (context, snapshots) {
-            if(snapshots.data == null) return CircularProgressIndicator();
-
-            return ListView.builder(
-                itemCount: snapshots.data.documents.length,
-                itemBuilder:(context, index){
-                  DocumentSnapshot pdf= snapshots.data.documents[index];
-                  return ListTile(
-
-                    leading:PDF().cachedFromUrl(url),
-                    title: Text(pdf["name"]),
-                  );
-                }
-
-
-            );
-
-          })
-    );
-  }}
+        appBar: AppBar(
+            title: Text('Fwatery'), backgroundColor: Colors.orangeAccent),
+        body: SafeArea(
+          child: Center(
+              child: CircularProgressIndicator(
+            valueColor: new AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
+          )),
+        ));
+  }
+}
