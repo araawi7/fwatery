15e68b3bd2294be05bf68c92651d34f7b0b6d1a9
araawi7 <62933812+araawi7@users.noreply.github.com>
2020-12-07 15:33:21 +0300

    Update custom_routes.dart

--- lib/routes/custom_routes.dart
+++ lib/routes/custom_routes.dart
@@ -1,5 +1,7 @@
+// we use this file because it offer flexibility
 
 import 'package:flutter/material.dart';
+import 'package:fwatery/Pages/Viewpdf.dart';
 import 'package:fwatery/Pages/about_page.dart';
 import 'package:fwatery/Pages/contact_page.dart';
 import 'package:fwatery/Pages/first_page.dart';
@@ -12,8 +14,6 @@ import 'package:fwatery/Pages/pdf_view.dart';
 import 'package:fwatery/Pages/setting_page.dart';
 import 'package:fwatery/Pages/singup_page.dart';
 import 'package:fwatery/routes/route_name.dart';
-
-
 
 class CustomRoute{
   static Route<dynamic> allRoutes(RouteSettings settings){
@@ -28,19 +28,24 @@ class CustomRoute{
         return MaterialPageRoute(builder: (_)=> SignupPage());
       case mainPageRoute:
         return MaterialPageRoute(builder: (_)=> MainPage());
-      case foaterPageRoute:
-        return MaterialPageRoute(builder: (_)=> FoaterPage());
+      case forgetPageRoute:
+        return MaterialPageRoute(builder: (_)=> ForgetPage());
       case settingPageRoute:
         return MaterialPageRoute(builder: (_)=> SettingsPage());
       case contactPageRoute:
         return MaterialPageRoute(builder: (_)=> ContactPage());
       case aboutPageRoute:
         return MaterialPageRoute(builder: (_)=> AboutPage());
+      case foaterPageRoute:
+        return MaterialPageRoute(builder: (_)=> FoaterPage(url: settings.arguments),
+            settings: settings);
       case pdfUrlPageRoute:
         return MaterialPageRoute(
-            fullscreenDialog: true,
-            builder: (_) => PdfViewUrl(url: settings.arguments),
+            fullscreenDialog: true, builder: (_) => PdfViewUrl(url: settings.arguments),
             settings: settings);
+      case viewpdfPageRoute:
+        return MaterialPageRoute(
+            fullscreenDialog: true, builder: (_) => Viewpdf());
 
 
     }
