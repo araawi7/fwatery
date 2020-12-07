e3e28216a513e31fe62d1cc5d8e272f0843b4909
araawi7 <62933812+araawi7@users.noreply.github.com>
2020-12-07 15:31:54 +0300

    Update fwatery_page.dart

--- lib/Pages/fwatery_page.dart
+++ lib/Pages/fwatery_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:fwatery/Pages/Viewpdf.dart';
 
 class FoaterPage extends StatefulWidget {
   final String url;
@@ -10,18 +13,65 @@ class FoaterPage extends StatefulWidget {
 
 class _FoaterPageState extends State<FoaterPage> {
   final String url;
-
   _FoaterPageState(this.url);
+  //List<Modal> itemlist = List();
+
+  final mainReference = FirebaseDatabase.instance.reference().child("Database");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('Fwatery'), backgroundColor: Colors.orangeAccent),
-        body: SafeArea(
-          child: Center(
-              child: CircularProgressIndicator(
-            valueColor: new AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
-          )),
-        ));
+      appBar: AppBar(title: Text('Fwatery'), backgroundColor: Colors.orangeAccent),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('PDFs').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: new AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
+                  ));
+            }else{
+            return ListView(
+
+              children: snapshot.data.docs.map((document) {
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                    child: Container(
+                      width: 400,
+                      height: 90,
+                      child: Card(
+
+                        child:
+                        GestureDetector(
+
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(100, 40, 100, 0),
+                                  child: Text(document["PdfName"],style: TextStyle(color: Colors.orangeAccent,fontSize: 20,),),
+                                ),
+
+                                ],
+                            ),
+                            onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Viewpdf(),
+                                  settings: RouteSettings(
+                                      arguments: document["PdfLink"])));
+                        }
+
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            );}
+          }),
+    );
   }
 }
