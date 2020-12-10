c52472229144ffa0c4f1673929b69bfb76afe367
araawi7 <62933812+araawi7@users.noreply.github.com>
2020-12-10 16:48:15 +0300

    Update pdf_view.dart

--- lib/Pages/pdf_view.dart
+++ lib/Pages/pdf_view.dart
@@ -1,15 +1,11 @@
 //the page will call pdf method
-import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dio/dio.dart';
-import 'package:ext_storage/ext_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 import 'package:flutter_share/flutter_share.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class PdfViewUrl extends StatefulWidget {
   final String url;
@@ -21,7 +17,6 @@ class PdfViewUrl extends StatefulWidget {
 
 class _PdfViewUrlState extends State<PdfViewUrl> {
   final String url;
-  var dio = Dio();
 
   _PdfViewUrlState(this.url);
   // ignore: deprecated_member_use
@@ -33,17 +28,7 @@ class _PdfViewUrlState extends State<PdfViewUrl> {
       appBar: AppBar(
         automaticallyImplyLeading: true,
         actions: [
-          IconButton(icon: Icon(Icons.share), onPressed: share),
-          IconButton(
-            icon: Icon(Icons.save_alt),
-            onPressed: () async {
-              String path = await ExtStorage.getExternalStoragePublicDirectory(
-                  ExtStorage.DIRECTORY_DOWNLOADS);
-              String fullPath = "$path/.pdf";
-              download2(dio, url, fullPath);
-            },
-          ),
-        ],
+          IconButton(icon: Icon(Icons.share), onPressed: share),],
         title: Text(
           "Fwatery",
           style: TextStyle(color: Colors.white),
@@ -72,48 +57,7 @@ class _PdfViewUrlState extends State<PdfViewUrl> {
     );
   }
 
-  @override
-// ignore: must_call_super
-  void initState() {
-    getPermission();
-  }
 
-  void getPermission() async {
-    print("    get Permission");
-    await PermissionHandler().requestPermissions([PermissionGroup.storage]);
-  }
-
-  Future download2(Dio dio, String url, String savePath) async {
-    //get pdf from link
-    try {
-      Response response = await dio.get(
-        url,
-        onReceiveProgress: showDownloadProgress,
-        //Received data with List<int>
-        options: Options(
-            responseType: ResponseType.bytes,
-            followRedirects: false,
-            validateStatus: (status) {
-              return status < 500;
-            }),
-      );
-
-      //write in download folder
-      File file = File(savePath);
-      var raf = file.openSync(mode: FileMode.write);
-      raf.writeFromSync(response.data);
-      await raf.close();
-    } catch (e) {
-      print("error");
-      print(e);
-    }
-  }
-
-  void showDownloadProgress(received, total) {
-    if (total != -1) {
-      print((received / total * 100).toStringAsFixed(0) + "%");
-    }
-  }
 
   Future<void> share() async {
     await FlutterShare.share(
